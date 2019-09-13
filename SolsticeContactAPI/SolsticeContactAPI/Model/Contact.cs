9e3cb4728f297bcb00af53768abcdc2fb667a137
KevinMa0207 <kevin.ma0207@gmail.com>
2019-09-12 23:03:07 -0500

    initial project
    initial project

---
+++ SolsticeContactAPI/SolsticeContactAPI/Model/Contact.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolsticeContactAPI.Model
+{
+    public class Contact
+    {
+    }
+}
---
+++ SolsticeContactAPI/SolsticeContactAPItest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace SolsticeContactAPItest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
